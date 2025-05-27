74ca945264cecd8bd484284fbefe12eb7d79b09b
Matheus Gomes F. Eusebio <matt-xp2@live.com>
2025-05-27 11:02:34 -0300

    Realizado a questão 03

---
+++ Exercicio_03/Models/Produto.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_03.Models
+{
+    public class Produto
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+
+        private static List<Produto> produtos = new List<Produto>();
+
+        public Produto()
+        {
+            
+        }
+        public Produto(int codigo, string nome, decimal preco)
+        {
+            Codigo = codigo;
+            Nome = nome;
+            Preco = preco;
+        }
+
+        public void Adicionar(Produto p)
+        {
+            produtos.Add(p);
+        }
+        public bool Remover(int codigo)
+        {
+            if (produtos.Any(x => x.Codigo == codigo))
+            {
+                var produtoRemover = produtos.Find(x => x.Codigo == codigo);
+                produtos.Remove(produtoRemover);
+                return true;
+            }
+            return false;
+        }
+        public string BuscarPorCodigo(int codigo)
+        {
+            if (produtos.Any(x => x.Codigo == codigo))
+            {
+                StringBuilder sb = new StringBuilder();
+                var produtoImprimir = produtos.Find(x => x.Codigo == codigo);
+
+                sb.AppendLine($"Código do produto = {produtoImprimir.Codigo}");
+                sb.AppendLine($"Nome do Produto = {produtoImprimir.Nome}");
+                sb.AppendLine($"Preco do Produto = {produtoImprimir.Preco:C}");
+                return sb.ToString();
+            }
+
+            return $"Produto não cadastrado";
+        }
+
+        public string ListarTodos()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in produtos)
+            {
+                sb.AppendLine($"Código do produto = {item.Codigo}");
+                sb.AppendLine($"Nome do Produto = {item.Nome}");
+                sb.AppendLine($"Preco do Produto = {item.Preco:C}");
+                sb.AppendLine();
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Exercicio_03/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Exercicio_03.Models;
+
+namespace Teste_Inga;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Produto p = new Produto();
+
+        bool exit = false;
+
+        while (!exit)
+        {
+            Console.Clear();
+            Console.WriteLine("==== MENU PRODUTOS ====");
+            Console.WriteLine("[1] - ADICIONAR PRODUTO");
+            Console.WriteLine("[2] - REMOVER PRODUTO");
+            Console.WriteLine("[3] - LISTAR PRODUTO");
+            Console.WriteLine("[4] - BUSCAR PRODUTO");
+            Console.WriteLine("[5] - SAIR");
+            Console.Write("Digite uma das opções: ");
+            string choice = Console.ReadLine();
+
+            Console.Clear();
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("==== MENU ADICIONAR TAREFA ====");
+                    Console.Write("Adicione o código do produto: ");
+                    int codigo = IsValidInt(Console.ReadLine());
+
+                    Console.Write("Adicione o nome do produto: ");
+                    string nome = Console.ReadLine();
+
+                    Console.Write("Adicione o Preco do produto: R$ ");
+                    decimal priceProduct = IsValidDecimal(Console.ReadLine());
+
+                    Produto produto = new Produto(codigo, nome, priceProduct);
+                    produto.Adicionar(produto);
+
+                    Console.WriteLine("Pressione qualquer tecla para voltar ao menu");
+                    Console.ReadLine();
+                    break;
+                case "2":
+                    Console.WriteLine("==== MENU REMOÇÃO PRODUTO ====");
+                    Console.Write("Digite o código do produto que vai ser removido: ");
+                    int idProduto = IsValidInt(Console.ReadLine());
+
+                    string produtoRemovido = p.Remover(idProduto) ? "Produto removido com sucesso" : "Produto não encontrado";
+                    Console.WriteLine(produtoRemovido);
+
+                    Console.WriteLine("Pressione qualquer tecla para voltar ao menu");
+                    Console.ReadLine();
+                    break;
+                case "3":
+                    Console.WriteLine("==== MENU DE PRODUTOS CADASTRADOS ====");
+                    Console.WriteLine(p.ListarTodos());
+                    Console.WriteLine("Pressione qualquer tecla para voltar ao menu");
+                    Console.ReadLine();
+                    break;
+                case "4":
+                    Console.WriteLine("==== MENU DE BUSCAR PRODUTOS ====");
+                    Console.Write("Digite o código do produto que vai ser buscado: ");
+                    int idProcurar = IsValidInt(Console.ReadLine());
+                    Console.WriteLine(p.BuscarPorCodigo(idProcurar));
+                    Console.WriteLine("Pressione qualquer tecla para voltar ao menu");
+                    Console.ReadLine();
+                    break;
+                case "5":
+                    exit = true;
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida!!! Digite qualquer tecla para voltar ao menu principal");
+                    Console.ReadLine();
+                    break;
+            }
+        }
+        Console.WriteLine("Obrigado por usar o programa!!!");
+    }
+
+    static int IsValidInt(string inputUser)
+    {
+        bool sucess = int.TryParse(inputUser, out int number);
+        while (!sucess)
+        {
+            Console.Write("Digite um número válido: ");
+            sucess = int.TryParse(Console.ReadLine(), out number);
+        }
+
+        return number;
+    }
+
+    static decimal IsValidDecimal(string inputUser)
+    {
+        bool sucess = decimal.TryParse(inputUser, out decimal number);
+        while (!sucess)
+        {
+            Console.Write("Digite um valor válido: ");
+            sucess = decimal.TryParse(Console.ReadLine(), out number);
+        }
+
+        return number;
+    }
+}
